b1fe434ff6df72c0c018cfb2e9ef39debdf02a0c
brendaevilly <brendaevilly014@gmail.com>
2026-03-25 10:45:24 -0300

    exemplo clássico de algoritmo de ordenação em pares: bubble sort

--- comparacao_em_pares.py
+++ comparacao_em_pares.py
@@ -1,67 +1,26 @@
-import random
 
-def cria_lista(tamanho): 
-    lista = []  
+# exemplo clássico de algoritmo de ordenação em pares: bubble sort
+
+def bubble_sort(lista):
+    tamanho = len(lista)
     for i in range(tamanho):
-        lista.append(random.randint(0, 1000000000))
-
+        for j in range(0, tamanho-i-1):
+            # comparação em pares
+            if lista[j] > lista[j+1]:
+                #troca
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+                
     return lista
-
-def eh_primo(num):
-    if num <= 1:
-        return False
-    # testar divisores de 2 até a metade do número
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-# faz todos os pares possíveis
-def testar_todos_os_pares(lista):
-    resultados = []
-    for i in range(len(lista)):
-        for j in range(i+1, len(lista)):
-            resultados.append((lista[i], lista[j]))
-
-    return resultados
-
-# força bruta condicional, encontra para em que o resultado da soma é um número primo
-def forca_bruta_condicional(lista):
-    resultados = []
-    for i in range(len(lista)):
-        for j in range(i+1, len(lista)):
             
-            soma = lista[i] + lista[2]
-            if soma > 1 and eh_primo(soma):
-                resultados.append((lista[i], lista[j]))
-
-    return resultados
-
-# exemplo de problema otimizado: soma
-def soma_otimizada(lista, alvo):
-    vistos = set()
-    
-    for num in lista:
-        if (alvo - num) in vistos:
-            return num, alvo - num
-        vistos.add(num)
-    
-    return None
-
 if __name__ == "__main__":
-    lista = cria_lista(10000) # pode alterar o tamanho da lista nos testes
+    lista1 = [3, 7, 2, 9, 5, 4, 8, 11, 10]
+    lista2 = [10, 4, 6, 8, 1, 15, 3, 12, 1, 2, 7, 11, 5, 14]
     
-    resultado = testar_todos_os_pares(lista)
-    #resultado = forca_bruta_condicional(lista)
-    
-    for i in resultado:
-        print(i)
-        
-    print(len(resultado))
-    
-    
-    """
-    resultado = soma_otimizada(lista, 7)
-    print("Resultado: ", resultado)
-    """
+    print("Vetor 1 antes: ", lista1)
+    print("Vetor 2 antes: ", lista2)
+    v1 = bubble_sort(lista1)
+    v2 = bubble_sort(lista2)
+    print(" ")
+    print("Vetor 1 depois: ", v1)
+    print("Vetor 2 depois: ", v2)
     
